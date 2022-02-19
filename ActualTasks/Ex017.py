cfb8600ffe464d8960a0cb59705092f6543d297b
AVoit <AVoit@myemail.com>
2022-02-19 22:29:21 +0500

    ex017

--- ActualTasks/Ex017.py
+++ ActualTasks/Ex017.py
@@ -1,5 +1,29 @@
 # 17. Задать список из N элементов, заполненных числами из [-N, N]. Найти произведение элементов на указанных позициях. 
 # Позиции хранятся в файле file.txt в одной строке одно число
+list = []
+n = int(input('N = '))
+for i in range (-n, n+1):
+    list.append(i)
+#print(list)
+
+res = 1
+with open('Ex017.txt', 'r') as data:
+    for line in data:
+      i = int(line)
+      res = res * list[i]
+print(res)
+
+# res = 1
+# path = 'Ex017.txt'
+# data = open(path, 'r')
+# for line in data:
+#     i = int(line)
+#     #print(line)
+#     res = res * list[i]
+# data.close()
+# print(res)
+
+
 
 
 
